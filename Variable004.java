ba6c66f4fd56a4130adf184a69a67d9e0c6fcecf
MRidho-Saputra <ridhoindoid@gmail.com>
2026-09-21 22:33:30 +0800

    Create Variable004.java

---
+++ Variable004.java
@@ -0,0 +1,23 @@
+package Variable;
+public class Variable004 {
+    public static void main(String[] args) {
+        //deklarasi variabel
+        int Bilangan1, Bilangan2;
+        int hasil_jumlah, hasil_kurang, hasil_kali, hasil_bagi;
+
+        Bilangan1 = 10;
+        Bilangan2 = 2;
+
+        hasil_jumlah = Bilangan1 + Bilangan2;
+        hasil_kurang = Bilangan1 - Bilangan2;
+        hasil_kali = Bilangan1 * Bilangan2;
+        hasil_bagi = Bilangan1 / Bilangan2;
+
+        System.out.println(hasil_jumlah);
+        System.out.println(hasil_kurang);
+        System.out.println(hasil_kali);
+        System.out.println(hasil_bagi);
+        
+
+    }
+}
